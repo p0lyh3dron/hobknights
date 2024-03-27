c624bbf9d3d8e5f0892dca098ec2477cd8249fae
demez <demizexp@gmail.com>
2024-03-26 22:46:00 -0400

    modify drive system

--- src/game/drive.c
+++ src/game/drive.c
@@ -2,13 +2,14 @@
 
 #include <string.h>
 
-#define PLAYERSPEED 20
+#define PLAYERSPEED 60
 
 extern int _active;
 
 e_transform_t *_trans = nullptr;
 e_driveable_t *_drive = nullptr;
 
+unsigned char _mouse_capture = 1;
 unsigned char _grounded = 0;
 unsigned char _noclip   = 0;
 
@@ -46,38 +47,50 @@ void drive_drive_driven_entity() {
         if (strncmp(pEvent, "esc\0", 4) == 0) {
             _active = 0;
         }
-        if (strncmp(pEvent, "w\0", 2) == 0) {
-            _drive->velocity.z +=
+        else if (strncmp(pEvent, "`\0", 2) == 0) {
+            if (_mouse_capture) {
+                _mouse_capture = 0;
+                // crashes???
+                // platform_set_mouse_capture(0);
+            }
+            else {
+                _mouse_capture = 1;
+                // platform_set_mouse_capture(1);
+            }
+        }
+        else if (strncmp(pEvent, "w\0", 2) == 0) {
+            _trans->pos.z +=
                 0.1f * cosf(_trans->rot.y) *
-                cosf(_trans->rot.x) * PLAYERSPEED * base_get_dt() * 2500;
-            _drive->velocity.x -=
+                cosf(_trans->rot.x) * PLAYERSPEED * base_get_dt();
+            _trans->pos.x -=
                 0.1f * sinf(_trans->rot.y) *
-                cosf(_trans->rot.x) * PLAYERSPEED * base_get_dt() * 2500;
+                cosf(_trans->rot.x) * PLAYERSPEED * base_get_dt();
             if (_drive->move_type == DRIVE_NOCLIP)
-                _trans->pos.y += 0.1f * sinf(_trans->rot.x) * PLAYERSPEED * base_get_dt() * 2500;
+                _trans->pos.y += 0.1f * sinf(_trans->rot.x) * PLAYERSPEED * base_get_dt();
         }
-        if (strncmp(pEvent, "a\0", 2) == 0) {
-            _drive->velocity.x -=
-                0.1f * cosf(_trans->rot.y) * PLAYERSPEED * base_get_dt() * 2500;
-            _drive->velocity.z -=
-                0.1f * sinf(_trans->rot.y) * PLAYERSPEED * base_get_dt() * 2500;
+        else if (strncmp(pEvent, "a\0", 2) == 0) {
+            _trans->pos.x -=
+                0.1f * cosf(_trans->rot.y) * PLAYERSPEED * base_get_dt();
+            _trans->pos.z -=
+                0.1f * sinf(_trans->rot.y) * PLAYERSPEED * base_get_dt();
         }
-        if (strncmp(pEvent, "s\0", 2) == 0) {
-            _drive->velocity.z -=
+        else if (strncmp(pEvent, "s\0", 2) == 0) {
+            _trans->pos.z -=
                 0.1f * cosf(_trans->rot.y) *
-                cosf(_trans->rot.x) * PLAYERSPEED * base_get_dt() * 2500;
-            _drive->velocity.x +=
+                cosf(_trans->rot.x) * PLAYERSPEED * base_get_dt();
+            _trans->pos.x +=
                 0.1f * sinf(_trans->rot.y) *
-                cosf(_trans->rot.x) * PLAYERSPEED * base_get_dt() * 2500;
+                cosf(_trans->rot.x) * PLAYERSPEED * base_get_dt();
             if (_drive->move_type == DRIVE_NOCLIP)
-                _trans->pos.y -= 0.1f * sinf(_trans->rot.x) * PLAYERSPEED * base_get_dt() * 2500;
+                _trans->pos.y -= 0.1f * sinf(_trans->rot.x) * PLAYERSPEED * base_get_dt();
         }
-        if (strncmp(pEvent, "d\0", 2) == 0) {
-            _drive->velocity.x +=
-                0.1f * cosf(_trans->rot.y) * PLAYERSPEED * base_get_dt() * 2500;
-            _drive->velocity.z +=
-                0.1f * sinf(_trans->rot.y) * PLAYERSPEED * base_get_dt() * 2500;
+        else if (strncmp(pEvent, "d\0", 2) == 0) {
+            _trans->pos.x +=
+                0.1f * cosf(_trans->rot.y) * PLAYERSPEED * base_get_dt();
+            _trans->pos.z +=
+                0.1f * sinf(_trans->rot.y) * PLAYERSPEED * base_get_dt();
         }
+
         if (strncmp(pEvent, "v\0", 2) == 0) {
             if (_drive->move_type == DRIVE_NOCLIP && _noclip == 0) {
                 _drive->move_type = DRIVE_CLIP;
@@ -96,25 +109,35 @@ void drive_drive_driven_entity() {
         }
         if (strncmp(pEvent, "space\0", 6) == 0) {
             if (_drive->move_type == DRIVE_NOCLIP)
-                _trans->pos.y += 0.1f * PLAYERSPEED * base_get_dt() * 2500;
+                _trans->pos.y += 0.1f * PLAYERSPEED * base_get_dt();
             else if (_grounded) {
                 _drive->velocity.y = 2.f;
             }
         }
-        if (strncmp(pEvent, "lctrl\0", 7) == 0) {
+        else if (strncmp(pEvent, "lctrl\0", 7) == 0) {
             if (_drive->move_type == DRIVE_NOCLIP)
-                _trans->pos.y -= 0.1f * PLAYERSPEED * base_get_dt() * 2500;
+                _trans->pos.y -= 0.1f * PLAYERSPEED * base_get_dt();
         }
         pEvent = platform_get_event();
     }
-    vec2u_t mouse = platform_get_joystick_event();
-    _trans->rot.x -= (float)mouse.y / 500.f;
-    _trans->rot.y -= (float)mouse.x / 500.f;
 
-    _trans->rot.x =
-        MAX(_trans->rot.x, -3.14159f / 2.f);
-    _trans->rot.x =
-        MIN(_trans->rot.x, 3.14159f / 2.f);
+    if (_mouse_capture) {
+        static float m_pitch = 0.022f;
+        static float m_yaw = 0.022f;
+        float sens = args_get_float("--mouse-sens", 1.f);
+        sens *= m_yaw;
+
+        vec2u_t mouse = platform_get_joystick_event();
+        _trans->rot.x -= ((float)mouse.y * m_pitch * sens);
+        _trans->rot.y -= ((float)mouse.x * m_yaw * sens);
+        // _trans->rot.x -= ((float)mouse.y * m_pitch * sens) / 500.f;
+        // _trans->rot.y -= ((float)mouse.x * m_yaw * sens) / 500.f;
+
+        _trans->rot.x =
+            MAX(_trans->rot.x, -3.14159f / 2.f);
+        _trans->rot.x =
+            MIN(_trans->rot.x, 3.14159f / 2.f);
+    }
 
     if (_drive->move_type == DRIVE_CLIP) {
         _drive->acceleration.y = -.981f;
@@ -129,13 +152,13 @@ void drive_drive_driven_entity() {
         _drive->velocity.z *= sqrtf(2.5f/speed);
     }
 
-    _drive->velocity.x += _drive->acceleration.x * base_get_dt() * 2500;
-    _drive->velocity.y += _drive->acceleration.y * base_get_dt() * 2500;
-    _drive->velocity.z += _drive->acceleration.z * base_get_dt() * 2500;
+    _drive->velocity.x += _drive->acceleration.x * base_get_dt();
+    _drive->velocity.y += _drive->acceleration.y * base_get_dt();
+    _drive->velocity.z += _drive->acceleration.z * base_get_dt();
 
-    _trans->pos.x += _drive->velocity.x * base_get_dt() * 2500;
-    _trans->pos.y += _drive->velocity.y * base_get_dt() * 2500;
-    _trans->pos.z += _drive->velocity.z * base_get_dt() * 2500;
+    _trans->pos.x += _drive->velocity.x * base_get_dt();
+    _trans->pos.y += _drive->velocity.y * base_get_dt();
+    _trans->pos.z += _drive->velocity.z * base_get_dt();
     /*
         *    Clip movement is not yet implemented.
         */
